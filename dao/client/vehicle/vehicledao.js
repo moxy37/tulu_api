3e72cbd5914a8db450e2ca145a1b0c6096611269
verticaldean <dean.brennan@verticalmotion.ca>
2020-09-29 02:30:05 -0600

    Making dummy list

--- dao/client/vehicle/vehicledao.js
+++ dao/client/vehicle/vehicledao.js
@@ -4,6 +4,18 @@ module.exports = VehicleDAO;
 function VehicleDAO() {
 	this.list = function (tokenId, obj, next) {
 		var list = [];
+		var v1 = new Object();
+		v1.make = 'Ford';
+		v1.model = 'Focus';
+		v1.year = '2010';
+		v1.vin = '1234567890';
+		list.push(v1);
+		var v2 = new Object();
+		v2.make = 'Ford';
+		v2.model = 'Fusion';
+		v2.year = '2019';
+		v2.vin = '0201010';
+		list.push(v2);
 		return next(null, list);
 	}
 
