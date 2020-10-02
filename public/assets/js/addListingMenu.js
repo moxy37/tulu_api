f278bffa625af35201daf8f0e89b4e28393910fc
Lord Tapulao <55147167+LTapsy@users.noreply.github.com>
2020-10-02 14:13:25 -0600

    vin input, vin car info cars with delete button, add photos input

--- public/assets/js/addListingMenu.js
+++ public/assets/js/addListingMenu.js
@@ -5,49 +5,69 @@ const addPostingBtn = document.querySelector('.addPostingBtn');
 const addListingMenu = () => {
     
     if(addListingSetting=="off"){
-        addPostingContainer.style = "height:530px;transition:0.5s ease-in-out;";
+        addPostingContainer.style = "height:80vh;transition:0.5s ease-in-out;";
         addPostingBtn.style = "transform:rotate(405deg);transition:0.8s ease-in-out;";
         addListingSetting="on";
-        setTimeout(appear, 600);
     }else{
-        addPostingContainer.style = "height:70px;transition:0.5s ease-in-out;";
+        addPostingContainer.style = "height:10vh;transition:0.5s ease-in-out;";
         addPostingBtn.style = "transform:rotate(0deg);transition:0.8s ease-in-out;";
         addListingSetting="off";
-        setTimeout(reset, 100);
     }
 };
 
 
 const addVehicleStepOne = document.querySelector('.addVehicleStepOne');
+const addVehicleStepTwo = document.querySelector('.addVehicleStepTwo');
+const addVehicleStepThree = document.querySelector('.addVehicleStepThree');
 const vehicleAdded = document.querySelector('.vehicleAdded');
-
-const submitVehicle = () => {
+var addVehicleStepCount = 1;
+const addVehicleStep = () => {
+    if(addVehicleStepCount == 1){
         addVehicleStepOne.style = "display:Flex; opacity:0; transition:0.5s;";
+        addVehicleStepTwo.style = "display:Flex;";
+        addVehicleStepCount++;
+        setTimeout(showAddVehicleStepTwo, 600);
+    }else if(addVehicleStepCount == 2){
+        addVehicleStepTwo.style = "display:Flex; opacity:0; transition:0.5s;";
+        addVehicleStepThree.style = "display:Flex;";
+        addVehicleStepCount++;
+        setTimeout(showAddVehicleStepThree, 600);
+    }else if(addVehicleStepCount == 3){
+        addVehicleStepThree.style = "display:Flex; opacity:0; transition:0.5s;";
         vehicleAdded.style = "display:Flex;";
-        setTimeout(doneAdding, 600);
-};
-
-const doneAdding = () => {
-    vehicleAdded.style = "display:Flex; opacity:1; transition:0.5s;";
-    addVehicleStepOne.style = "display:none;";
-    
+        addVehicleStepCount++;
+        setTimeout(addedSuccessful, 600);
+    }
+    else if(addVehicleStepCount == 4){
+        vehicleAdded.style = "display:Flex; opacity:0; transition:0.5s;";
+        setTimeout(addingDone, 600);
+    }
 };
 
 const showAddVehicleStepOne = () => {
-    addPostingContainer.style = "height:70px;;transition:0.5s ease-in-out;";
-    addPostingBtn.style = "transform:rotate(0deg);transition:0.8s ease-in-out;";
-    addListingSetting="off";
-    vehicleAdded.style = "display:Flex; opacity:0; transition:0.5s;";
-    setTimeout(reset, 500);
-    
-}
-
-const reset = () => {
-    addVehicleStepOne.style = "display:Flex; opacity:0; transition:0.5s;";
-    vehicleAdded.style = "display:none;";
-}
-
-const appear = () => {
     addVehicleStepOne.style = "display:Flex; opacity:1; transition:0.5s;";
     vehicleAdded.style = "display:none;";
+}
+
+const addingDone = () => {
+    addPostingContainer.style = "height:10vh;transition:0.5s ease-in-out;";
+    addPostingBtn.style = "transform:rotate(0deg);transition:0.8s ease-in-out;";
+    addListingSetting="off";
+    addVehicleStepCount=1;
+    setTimeout(showAddVehicleStepOne, 600);
+}
+
+const showAddVehicleStepTwo = () => {
+    addVehicleStepTwo.style = "display:Flex; opacity:1; transition:0.5s;";
+    addVehicleStepOne.style = "display:none";
+}
+
+const showAddVehicleStepThree = () => {
+    addVehicleStepThree.style = "display:Flex; opacity:1; transition:0.5s;";
+    addVehicleStepTwo.style = "display:none;";
+}
+
+const addedSuccessful = () => {
+    vehicleAdded.style = "display:Flex; opacity:1; transition:0.5s;";
+    addVehicleStepThree.style = "display:none;";
 }
