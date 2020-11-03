dab8efca4dd41c802dd270fdce4de1ba85aaf87a
verticaldean <dean.brennan@verticalmotion.ca>
2020-11-02 21:33:48 -0700

    Trying one more error catch.

--- public/assets/js/vendorview.js
+++ public/assets/js/vendorview.js
@@ -16,7 +16,13 @@ function populateVehicle(gVehicle) {
 		for (var i = 0; i != vehicle.length; i++) {
 			html = html + '<li class="activeListingListItem" id="' + vehicle[i].vin + '">';
 			var text = '';
-			if (vehicle[i].links !== undefined && vehicle[i].links.length > 0) { text = vehicle[i].links[0].url; }
+			if (vehicle[i].links !== undefined) {
+				if (vehicle[i].links[0] !== undefined) {
+					if (vehicle[i].links[0].url !== undefined) {
+						text = vehicle[i].links[0].url;
+					}
+				}
+			}
 			html = html + '    <img src="' + text + '" alt="" class="listingImage">';
 			html = html + '    <div class="listingInfo">';
 			html = html + '        <h4 class="listingName">' + vehicle[i].year + ' ' + vehicle[i].make + ' ' + vehicle[i].model + '</h4>';
