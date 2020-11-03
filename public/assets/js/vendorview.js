cf85aa377aaeea947b0557ab4db76254f4be01d0
verticaldean <dean.brennan@verticalmotion.ca>
2020-11-02 21:30:56 -0700

    Pushing error fix.

--- public/assets/js/vendorview.js
+++ public/assets/js/vendorview.js
@@ -10,14 +10,16 @@ function PageLoadFunction() {
 }
 
 function populateVehicle(gVehicle) {
-    var dealerId = 'dfb56be7-15ef-11eb-83a2-e86a647a411d';
-    DisplayVehicle(dealerId).then(function (vehicle) {
+	var dealerId = 'dfb56be7-15ef-11eb-83a2-e86a647a411d';
+	DisplayVehicle(dealerId).then(function (vehicle) {
 		var html = '';
 		for (var i = 0; i != vehicle.length; i++) {
-			html = html + '<li class="activeListingListItem" id="'+vehicle[i].vin+'">';
+			html = html + '<li class="activeListingListItem" id="' + vehicle[i].vin + '">';
+			var text = '';
+			if (vehicle[i].links !== undefined && vehicle[i].links.length > 0) { text = vehicle[i].links[0].url; }
 			html = html + '    <img src="' + vehicle[i].links[0].url + '" alt="" class="listingImage">';
 			html = html + '    <div class="listingInfo">';
-			html = html + '        <h4 class="listingName">' + vehicle[i].year +' '+ vehicle[i].make +' '+ vehicle[i].model + '</h4>';
+			html = html + '        <h4 class="listingName">' + vehicle[i].year + ' ' + vehicle[i].make + ' ' + vehicle[i].model + '</h4>';
 			html = html + '        <p class="listingPrice">$' + vehicle[i].msrp + '</p>';
 			html = html + '        <p  class="listingViews">4420 views</p>';
 			html = html + '    </div>';
@@ -32,5 +34,5 @@ function populateVehicle(gVehicle) {
 		$("#ActiveListing").append(html);
 	});
 
-	
+
 }
