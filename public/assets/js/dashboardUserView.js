f3ee01b137a01f7d6d494f666889ef571942905a
LTapsy <drolxiv@gmail.com>
2020-11-16 15:41:40 -0700

    alerts on click

--- public/assets/js/dashboardUserView.js
+++ public/assets/js/dashboardUserView.js
@@ -11,3 +11,23 @@ function showAddNewUser(){
 function hideAddNewUser(){
 	document.querySelector(".addNewUser").style = "display:none;";
 }
+
+function showEditUser(){
+	document.querySelector(".editUser").style = "display:flex;";
+}
+
+function hideEditUser(){
+	document.querySelector(".editUser").style = "display:none;";
+}
+
+function saveEdit(){
+	alert('Save Edit');
+}
+
+function saveUser(){
+	alert('Save User');
+}
+
+function deleteUser(){
+	alert('Delete User');
+}
